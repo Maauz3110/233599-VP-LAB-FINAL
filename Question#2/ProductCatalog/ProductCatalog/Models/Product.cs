ea38e1115126b1b541b05821407dcacfe0baa6a9
Muhammad Maauz Mansoor <muhammadmaauz2018@gmail.com>
2025-01-08 15:48:10 +0500

    Add files via upload

---
+++ Question#2/ProductCatalog/ProductCatalog/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductCatalog.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
